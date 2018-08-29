0f157e08b822dc17ef66bf9ab11dcf4a4ab5ab34
garfield <admin@g4rf.net>
2018-08-29 09:41:01 +0200

    basic html structure less/css stuff installed gettext

---
+++ js/main.js
@@ -0,0 +1,5 @@
+/*** do some good stuff from Boilerplate ***/
+MBP.hideUrlBarOnLoad(); // Hide URL Bar
+MBP.preventZoom(); // Prevent iOS from zooming onfocus
+MBP.startupImage(); // iOS Startup Image helper
+
